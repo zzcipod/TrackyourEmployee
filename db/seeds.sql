78b7164bf967fa46cbf33b173b858732e0d301ba
zzcipod <jonzzheng@gmail.com>
2022-08-16 18:27:11 -0700

    cc

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,28 +19,21 @@
 INSERT INTO department (department_name)
 VALUES ("Accounting"),
         ("Marketing"),
-        ("IT"),
-        ("Legal"),
-        ("Research and Developement");
+        ("Management"),
+        ("IT");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES  ("Accountant", 95000, 1),
-        ("Social Media Intern", 25000, 2),
-        ("Developer", 125000, 3),
-        ("Counsel", 195000, 4),
-        ("Researcher", 75000, 5);
+VALUES  ("Accountant", 60000, 1),
+        ("Agent", 80000, 2),
+        ("Manager", 120000, 3),
+        ("Engineer", 120000, 4),
+        
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Alice', 'Fannybottoms', 2, null),
-       (2, 'Luke', 'Skywalker', 3, null),
-       (3, 'Greg', 'Siggins', 3, 2),
-       (4, 'Jenna', 'Buettner', 1, null),
-       (5, 'Harry', 'Potter', 5, null),
-       (6, 'Lizzie', 'McGuire', 5, 5),
-       (7, 'Nick', 'Pisano', 1, 4),
-       (9, 'Hannah', 'Montana', 5, null),
-       (10, 'Oliver', 'Sykes', 5, 9),
-       (11, 'Jason', 'Earles', 4, null),
-       (12, 'Selena', 'Gomez', 4, 11);
-
-       
+VALUES (1, 'Harry', 'Potter', 2, null),
+       (2, 'Ron', 'Weasley', 3, null),
+       (3, 'Lord', 'Voldemort', 3, 2),
+       (4, 'Hermione', 'Granger', 1, null),
+       (5, 'Ginny', 'Weasley', 1, null),
+       (6, 'Draco', 'Malfoy', 4, 5),
+       (7, 'Neville', 'Longbottom', 2, 4),
