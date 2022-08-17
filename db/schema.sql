cf1c2b639c29ac4ca5838cb7fe18d158b465bdfd
zzcipod <jonzzheng@gmail.com>
2022-08-16 20:28:50 -0700

    loom

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,3 @@
--- DROP DATABASE IF EXISTS Tracker_db;
--- CREATE DATABASE Tracker_db;
-
--- USE Tracker_db;
-
--- CREATE TABLE department_table (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   department_name VARCHAR(30) NOT NULL
--- );
-
--- CREATE TABLE role_table (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     role_title VARCHAR(30) NOT NULL
---     salary DECIMAL,
---     department_id INT,
---     FOREIGN KEY (department_table)
---     REFERENCES movies(id)
---     ON DELETE SET NULL
--- );
-
--- CREATE TABLE employee_table (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     movie_id INT,
---     review TEXT NOT NULL,
---     FOREIGN KEY (movie_id)
---     REFERENCES movies(id)
---     ON DELETE SET NULL
--- );
-
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,3 @@
--- INSERT INTO department_table (department_name)
--- VALUES (Marketing),
---         (Accounting),
---         (Manucturing);
-
-
--- INSERT INTO role_table (role_title, role_salary, department_id)
--- VALUES ("Marketing", 100000, 1),
---         ("Accounting", 100000, 2),
---         ("Accounting", 100000, 2),
---         ("Manucturing", 100000, 3);
-
--- INSERT INTO employee_table (first_name, last_name, role_id, manager_id)
--- VALUES ("Tom", "Cruise", 1, null),
---        ("Tom", "Hanks", 1, 1),
---        ("Suri", "Cruise", 2, null),
---        ("Maria", "Carlery", 2, 3),
-
 INSERT INTO department (department_name)
 VALUES ("Accounting"),
         ("Marketing"),
@@ -24,16 +6,8 @@ VALUES ("Accounting"),
 
 INSERT INTO roles (title, salary, department_id)
 VALUES  ("Accountant", 60000, 1),
-        ("Agent", 80000, 2),
-        ("Manager", 120000, 3),
-        ("Engineer", 120000, 4),
+
         
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, 'Harry', 'Potter', 2, null),
-       (2, 'Ron', 'Weasley', 3, null),
-       (3, 'Lord', 'Voldemort', 3, 2),
-       (4, 'Hermione', 'Granger', 1, null),
-       (5, 'Ginny', 'Weasley', 1, null),
-       (6, 'Draco', 'Malfoy', 4, 5),
-       (7, 'Neville', 'Longbottom', 2, 4),
